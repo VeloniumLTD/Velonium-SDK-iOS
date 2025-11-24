f8a0f0d0e872be3637878f06f450833069ddc035
VeloniumLTD <ron@velonium.com>
2025-11-24 18:02:28 +0200

    Version 1.0.1

---
+++ VeloniumSDK.xcframework/ios-arm64/VeloniumSDK.framework/Headers/VeloniumSDK.h
@@ -0,0 +1,18 @@
+//
+//  VeloniumSDK.h
+//  VeloniumSDK
+//
+//  Created by mac on 13/11/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VeloniumSDK.
+FOUNDATION_EXPORT double VeloniumSDKVersionNumber;
+
+//! Project version string for VeloniumSDK.
+FOUNDATION_EXPORT const unsigned char VeloniumSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VeloniumSDK/PublicHeader.h>
+
+
